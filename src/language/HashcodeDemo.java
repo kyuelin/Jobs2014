e36010ed7df43d5cd0e24f08a88fbda50fd60c83
kyuelin <kyuelin@gmail.com>
2014-04-20 00:23:06 -0400

    First GitHub commit
    First GH commit

---
+++ src/language/HashcodeDemo.java
@@ -0,0 +1,15 @@
+package language;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class HashcodeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<String> sString = new HashSet<String>();
+		sString.add("123");		
+
+	}
+
+}
