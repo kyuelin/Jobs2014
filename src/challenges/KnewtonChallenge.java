1963e8c0eefbfe8c67daf47e81c66ae3f3b1e1e2
Kenneth <Kenneth@192.168.0.196>
2014-05-29 01:28:33 -0400

    use executor

--- src/challenges/KnewtonChallenge.java
+++ src/challenges/KnewtonChallenge.java
@@ -6,79 +6,69 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class KnewtonChallenge {
-	
-    private static Log logger = LogFactory.getLog(KnewtonChallenge.class);
+
+	private static Log logger = LogFactory.getLog(KnewtonChallenge.class);
 
 	// 1. read files
 	// 2. foreach line create a runnable task to add compounded names into a map
-	// 3. create executor 
+	// 3. create executor
 	// 4. create completionservice for executor
 	// 5. add runnable to executor
 	// 6. start processes
 	// 7. loop through map for name pair with count of lines > 50
 	public static void main(String[] args) {
-		
-		Path file=Paths.get("C:\\Users\\linken\\Downloads\\Artist_lists_small.txt");
+
+		Path file = Paths
+				.get("D:\\linken\\Workspace\\Jobs2014\\src\\challenges\\Artist_lists_small.txt");
 		Charset charset = Charset.forName("UTF-8");
-		List<String> lines = new ArrayList<String>(100);
-		
-		long start=System.currentTimeMillis();
-		
-		try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-		    String line = null;
-		    while ((line = reader.readLine()) != null) {
-		    	lines.add(line.trim());
-		    }
-		} catch (IOException x) {
-		    System.err.format("IOException: %s%n", x);
-		}
-		
-		Map<String, List<Integer>> map = new ConcurrentHashMap<String, List<Integer>>(); 
-		
-//		int nThreads=lines.size();
-//		ExecutorService executor = Executors.newFixedThreadPool(nThreads);
-		
+
+		long start = System.currentTimeMillis();
+
 		KnewtonChallenge kc = new KnewtonChallenge();
-		
-//		List<Thread> threadlist = new LinkedList<Thread>();
-		
-		for (int i=0; i<lines.size(); ++i) {
-//			executor.execute(kc.new MyRunnable(lines.get(i), map, i));
-//			Thread t = new Thread(kc.new MyRunnable(lines.get(i), map, i));
-//			t.start();
-//			threadlist.add(t);
-			kc.new MyRunnable(lines.get(i), map, i).run();
+		Map<String, List<Integer>> map = new ConcurrentHashMap<String, List<Integer>>();
+
+		int nThreads = 8;
+		ExecutorService executor = Executors.newFixedThreadPool(nThreads);
+
+		try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+			String line;
+			int i = 0;
+			while ((line = reader.readLine()) != null) {
+				executor.submit(kc.new MyRunnable(line.trim(), map, i++));
+			}
+		} catch (IOException x) {
+			System.err.format("IOException: %s%n", x);
 		}
-		
-/*		for(final Thread t : threadlist) {
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			
-		}*/
-		
-		Iterator<Entry<String, List<Integer>>> iterator = map.entrySet().iterator();
-		while(iterator.hasNext()) {
+
+		executor.shutdown();
+		while (!executor.isTerminated())
+			;
+
+		Iterator<Entry<String, List<Integer>>> iterator = map.entrySet()
+				.iterator();
+		while (iterator.hasNext()) {
 			Map.Entry<String, List<Integer>> entry = iterator.next();
 			if (entry.getValue().size() >= 50) {
-				logger.info(entry.getKey()+ ":" + entry.getValue().size());			
+				logger.info(entry.getKey() + ":" + entry.getValue().size());
 			}
 		}
-		logger.info("map size:"+map.entrySet().size());
-		logger.info("program completed in : " + Long.toString(System.currentTimeMillis()-start) + "seconds.");
+		logger.info("map size:" + map.entrySet().size());
+		logger.info("program completed in : "
+				+ Long.toString(System.currentTimeMillis() - start) + "ms.");
 	}
 
 	class MyRunnable implements Runnable {
@@ -86,33 +76,33 @@ public class KnewtonChallenge {
 		Map<String, List<Integer>> map;
 		String in;
 		Integer line_no;
-		
+
 		public MyRunnable(String input, Map<String, List<Integer>> m, int lno) {
-			map=m;
-			in=input;
-			line_no=new Integer(lno);
+			map = m;
+			in = input;
+			line_no = new Integer(lno);
 		}
-		
+
 		@Override
 		public void run() {
 			String[] inp = in.split(",");
-			for (int x=0; x<inp.length;x++) {
-				for (int y=x+1; y<inp.length; y++) {
-					String key = inp[x]+","+inp[y];
-						if (map.containsKey(key)) {
-							map.get(key).add(line_no);
-						}
-						else {
-							synchronized (map) {
-								List<Integer> list = new LinkedList<Integer>();
-								list.add(line_no);
-								map.put(key, list);		
+			Arrays.sort(inp);
+			for (int x = 0; x < inp.length; x++) {
+				for (int y = x + 1; y < inp.length; y++) {
+					String key = inp[x] + "," + inp[y];
+					if (map.containsKey(key)) {
+						map.get(key).add(line_no);
+					} else {
+						synchronized (map) {
+							List<Integer> list = new LinkedList<Integer>();
+							list.add(line_no);
+							map.put(key, list);
 						}
 					}
 				}
 			}
 		}
-		
+
 	}
 
 }
