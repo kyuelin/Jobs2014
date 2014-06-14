5d0de7d5f74744f38555a44b69c0f3b4c9674005
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:34:45 -0400

    Create BuilderObject.java

---
+++ src/dev/kyuelin/patterns/BuilderObject.java
@@ -0,0 +1,39 @@
+package patterns;
+
+public class BuilderObject {
+	
+	private final int id;
+	private String name;
+	
+	private BuilderObject(BuilderObjectBuilder b) {
+		id=b.id;
+		name=b.name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static class BuilderObjectBuilder {
+		private final int id;
+		private String name;
+		
+		public BuilderObjectBuilder(int i) {
+			id=i;
+		}
+		
+		public BuilderObjectBuilder name (String n) {
+			this.name=n;
+			return this;
+		}
+		
+		public BuilderObject build() {
+			return new BuilderObject(this);
+		}
+		
+	}
+}
