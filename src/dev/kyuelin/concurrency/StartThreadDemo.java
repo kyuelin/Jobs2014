eea1e8943794642967f03d1095c7455dfc9a4b3f
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:32:19 -0400

    Create StartThreadDemo.java

---
+++ src/dev/kyuelin/concurrency/StartThreadDemo.java
@@ -0,0 +1,26 @@
+package concurrency;
+
+public class StartThreadDemo {
+	
+	public static void main(String [] args) {
+		Thread startT = new Thread(new Task("starter"));
+		Thread runT = new Thread(new Task("runner"));
+		
+		startT.start();
+		runT.run();
+	}
+	
+	private static class Task implements Runnable {
+		private String caller;
+		
+		public Task(String name) {
+			this.caller=name;
+		}
+		
+		public void run() {
+			System.out.println("caller:"+caller+" in thread"+Thread.currentThread().getName());
+		}
+		
+	}
+
+}
