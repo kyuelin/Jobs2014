a9249927f23bf6c424a906c6f277a54ee415913d
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:36:35 -0400

    Create IPrinciple.java

---
+++ src/dev/kyuelin/principles/IPrinciple.java
@@ -0,0 +1,27 @@
+package principles;
+
+// interface segregation principle - good example
+public interface IPrinciple {
+	interface IWorkable {
+		public void work();
+	}
+	interface IFeedable {
+		public void eat();
+	}
+	class Worker implements IWorkable, IFeedable {
+		public void work() {}
+		public void eat() {}
+	}
+	class Robot implements IWorkable {
+		public void work() {}
+	}
+	class SuperWorker implements IWorkable, IFeedable {
+		public void work() {}
+		public void eat() {}
+	}
+	class Manager {
+		IWorkable worker;
+		public void setWorker(IWorkable w) {worker = w;}
+		public void manage() {worker.work();}
+	}
+}
