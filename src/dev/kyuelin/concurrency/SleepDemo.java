dcd1a5995bf31768c873b6d585353eb56924eed3
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:31:57 -0400

    Create SleepDemo.java

---
+++ src/dev/kyuelin/concurrency/SleepDemo.java
@@ -0,0 +1,30 @@
+package concurrency;
+
+public class SleepDemo {
+	private static boolean fileProcessed = false;
+
+	public static class Process1Thread extends Thread {
+		public void run() {
+			while (!fileProcessed) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+			}
+			System.out.println("File Processed, now upload file...");
+		}
+	}
+
+	public static class Process2Thread extends Thread {
+		public void run() {
+			System.out.println("Start Processing File...");
+			// do some processing on the files here...
+			fileProcessed = true;
+		}
+	}
+
+	public static void main(String[] args) {
+		new Process1Thread().start();
+		new Process2Thread().start();
+	}
+}
