c4d1b1ccfb3ead989fcbac63a15a7529f9cca552
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:34:19 -0400

    Create AdapterDemo.java

---
+++ src/dev/kyuelin/patterns/AdapterDemo.java
@@ -0,0 +1,42 @@
+package patterns;
+
+public class AdapterDemo {
+	
+	public class Adaptee {
+		public void method1() {}		
+	}	
+	public class Adapter {		
+		private Adaptee a = null;
+		public void injAdapter(Adaptee adp) {
+			a = adp;
+		}
+		public void method() {
+			a.method1();
+		}				
+	}
+	
+	
+	interface Adaptee1 {
+		public void method1();
+	}	
+	interface Adaptee2 {
+		public void method2();
+	}
+	class Adapter2 implements Adaptee1, Adaptee2 {
+		public void method() {
+			method1();
+			method2();
+		}
+		public void method2() {}
+		public void method1() {}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
