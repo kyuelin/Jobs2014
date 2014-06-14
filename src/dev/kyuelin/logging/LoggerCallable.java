8caa30d6af9bdba1cb09e402aa05fcccf4f86994
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:29:17 -0400

    Create LoggerCallable.java

---
+++ src/dev/kyuelin/logging/LoggerCallable.java
@@ -0,0 +1,31 @@
+package com.ms.linken.logging;
+
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerCallable implements Callable<Object> {
+
+	private static Logger logger = LoggerFactory.getLogger(LoggerCallable.class);
+
+	private int loop = 1;
+	
+	public LoggerCallable(int l) {
+		loop=l; 
+	}
+	
+	public void run() {
+	}
+
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		int i = loop;
+		while (i>0) {
+			logger.debug(i-- + ":debugging abcdefghijklmnopqstuvwxyz1234567890");
+		}
+		return null;
+	}
+
+}
