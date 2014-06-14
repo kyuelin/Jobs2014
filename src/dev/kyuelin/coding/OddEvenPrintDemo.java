11090bb3d03cd91d7ea46416a8be43363dcab0cf
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:28:18 -0400

    Create OddEvenPrintDemo.java

---
+++ src/dev/kyuelin/coding/OddEvenPrintDemo.java
@@ -0,0 +1,50 @@
+package coding;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OddEvenPrintDemo {
+
+	private static Logger logger = LoggerFactory
+			.getLogger(OddEvenPrintDemo.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		class CountThread implements Runnable {
+
+			private final int remain;
+			private AtomicInteger count;
+
+			CountThread(AtomicInteger c, int rem) {
+				count = c;
+				remain = rem;
+			}
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+					while (count.get() < 9) {
+						synchronized(count) {
+							if (count.get() % 2 != remain) {	
+								count.incrementAndGet();
+								logger.debug("count=" + count.toString());
+							}							
+						}
+					}					
+					logger.debug("done...");
+			}
+
+		}
+
+		AtomicInteger count = new AtomicInteger(0);		
+		new Thread(new CountThread(count, 0)).start();
+		new Thread(new CountThread(count, 1)).start();
+	}
+
+}
