62f874d9ce9dc5d260a4dc2b0cd7cd667a50554a
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 18:58:56 -0400

    Create ListReverse.java

---
+++ src/dev/kyuelin/algorithms/ListReverse.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListReverse {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List list = new LinkedList();
+	}
+
+}
