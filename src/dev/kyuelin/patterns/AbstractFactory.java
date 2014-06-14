9c9a599fdd87c749f21dd0a21dbbab73fea41f0b
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:34:08 -0400

    Create AbstractFactory.java

---
+++ src/dev/kyuelin/patterns/AbstractFactory.java
@@ -0,0 +1,36 @@
+package patterns;
+
+public class AbstractFactory {
+	public IFactory makeFactory(int type) {
+		switch (type) {
+		case 1:
+			return new FactoryOne();
+		case 2:
+			return new FactoryTwo();	
+		default:				
+			return null;
+		}
+	}
+	public interface IFactory {
+		public IProduct produce();
+	}	
+	public interface IProduct {
+		public void doSomething();
+	}
+	public class FactoryOne implements IFactory {
+		public IProduct produce() {return new ProductOne();}		
+	}
+	public class FactoryTwo implements IFactory {
+		public IProduct produce() {return new ProductTwo();}		
+	}
+	public class ProductOne implements IProduct {
+		public void doSomething() {;}
+	}
+	public class ProductTwo implements IProduct {
+		public void doSomething() {;}
+	}
+	
+	public static void main(String[] args) {
+		new AbstractFactory().makeFactory(1).produce().doSomething();
+	}
+}
