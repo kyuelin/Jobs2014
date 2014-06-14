1a8583612341b335fec0e5b88f2f82edddc46fe0
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:34:30 -0400

    Create BuilderDemo.java

---
+++ src/dev/kyuelin/patterns/BuilderDemo.java
@@ -0,0 +1,17 @@
+package patterns;
+
+
+public class BuilderDemo {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BuilderObject bo = new BuilderObject.BuilderObjectBuilder(12).name("twelve").build();
+		
+		System.out.println("bo:" + bo.getId()+"-"+bo.getName());
+	}
+
+}
+
