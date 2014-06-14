04ead190a79cd7f69d2d10630a44f44f0cfbd2b5
kyuelin <kyuelin@users.noreply.github.com>
2014-06-14 19:37:46 -0400

    Create Order.java

---
+++ src/dev/kyuelin/serialization/Order.java
@@ -0,0 +1,15 @@
+package serialization;
+
+import java.io.Serializable;
+
+public abstract class Order implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	int oid;
+	
+	abstract int doSomething();
+		
+}
