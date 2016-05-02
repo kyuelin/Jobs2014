4941e533c10ec5609f266505ca119041b3c20ccc
kyuelin <kyuelin@gmail.com>
2016-05-02 18:55:18 -0400

    .

---
+++ src/dev/kyuelin/progcreek/BTreeTraversal.java
@@ -0,0 +1,88 @@
+package dev.kyuelin.progcreek;
+
+import apple.laf.JRSUIUtils;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by linken on 4/29/16.
+ */
+public class BTreeTraversal {
+    public static void preorder(TreeNode root ) {
+        if (root == null)  return;
+        System.out.println(root.value);
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void preorder2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.add(root);
+        while(!stack.empty()) {
+            TreeNode node = stack.pop();
+            System.out.println(node.value);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+    }
+    public static ArrayList<Integer> preorder3(TreeNode root) {
+        ArrayList<Integer> retlist = new ArrayList<>();
+        if (root == null) return retlist;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while(!stack.empty()) {
+            TreeNode node = stack.pop();
+            retlist.add(node.value);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return retlist;
+    }
+
+    public static void breathfirstsearch(TreeNode root, int target) {
+        LinkedList<TreeNode> nqueue = new LinkedList<>();
+
+        nqueue.add(root);
+        while(!nqueue.isEmpty()) {
+            TreeNode node = nqueue.poll();
+            if (node.value == target) {
+                break;
+            }
+            else {
+                nqueue.add(root.left);
+                nqueue.add(root.right);
+            }
+        }
+    }
+    public static void kthlargest(TreeNode root, int k) {
+        findkthLargest(root, k, 0);
+    }
+    public static void findkthLargest(TreeNode root, int k, int c) {
+        if (root != null || c>=k) {
+            return;
+        }
+        findkthLargest(root.right, k, c);
+
+        c++;
+
+        if (c==k) {
+            System.out.println("found kth largest" + root.value);
+        }
+
+        findkthLargest(root.left, k,c );
+
+    }
+}
---
+++ src/dev/kyuelin/progcreek/LinkedListNode.java
@@ -0,0 +1,13 @@
+package dev.kyuelin.progcreek;
+
+/**
+ * Created by linken on 4/29/16.
+ */
+public class LinkedListNode {
+    int value;
+    LinkedListNode next;
+    LinkedListNode(int v) {
+        value=v;
+        next=null;
+    }
+}
