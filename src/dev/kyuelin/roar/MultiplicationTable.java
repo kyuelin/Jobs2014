4bca160153c6e85442cd73ca4886f040fb569b9a
kyuelin <Kenneth@mlocal>
2019-05-11 13:12:34 -0400

    Added sql for tbl_transactions;

--- src/dev/kyuelin/roar/MultiplicationTable.java
+++ src/dev/kyuelin/roar/MultiplicationTable.java
@@ -6,10 +6,11 @@ public class MultiplicationTable {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    int _max = 5, _min = 1;
 
     while (true) {
-      int m1 = (int) (Math.random() * 10);
-      int m2 = (int) (Math.random() * 10);
+      int m1 = (int) (Math.random() * _max) + _min;
+      int m2 = (int) (Math.random() * _max) + _min;
       System.out.println(m1 + "x" + m2 + "=");
       int result = scanner.nextInt();
       if (result != m1 * m2) {
